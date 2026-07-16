6d186c4507a53aee4e08ebe5618f5610a2dc777f
Janice Bailey <jbailey@silveredge-gs.com>
2026-07-16 15:17:54 -0400

    Refactor S3 audit agent to produce assessment results

--- s3_audit_agent.py
+++ s3_audit_agent.py
@@ -7,18 +7,42 @@ class S3AuditAgent:
         for bucket in normalized_data:
             if bucket["algorithm"]:
                 encryption_status = "Passed"
+                finding = "Encryption is Enabled"
+                severity = "Informational"
+
             else:
                 encryption_status = "Failed"
+                finding = "Encryption is not Enabled"
+                severity = "High"
+            encryption_assessment_result = {
+                "resource": bucket["name"],
+                "resource type": "S3 Bucket",
+                "check": "Encryption",
+                "finding": finding,
+                "status": encryption_status,
+                "severity": severity,
+                "evidence": bucket["algorithm"],
+            }
             if bucket["versioning"] == "NotConfigured":
                 version_status = "Failed"
+                finding = "Versioning is not Enabled"
+                severity = "High"
+
             else:
                 version_status = "Passed"
+                finding = "Versioning is Enabled"
+                severity = "Informational"
 
-            bucket_results = {
-                "name": bucket["name"],
-                "encryption status": encryption_status,
-                "version status": version_status,
+            versioning_assessment_result = {
+                "resource": bucket["name"],
+                "resource type": "S3 Bucket",
+                "check": "Versioning",
+                "finding": finding,
+                "status": version_status,
+                "severity": severity,
+                "evidence": bucket["versioning"],
             }
-            bucket_findings.append(bucket_results)
+            bucket_findings.append(encryption_assessment_result)
+            bucket_findings.append(versioning_assessment_result)
 
         return bucket_findings
